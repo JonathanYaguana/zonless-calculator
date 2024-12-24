d0dee0b1bdf3441bb9443ef8b4079337ec86c04e
JonathanYaguana <jonathansacrox93@gmail.com>
2024-12-24 18:12:24 -0500

    host property & viewChildren

--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, viewChildren } from '@angular/core';
 import { CalculatorBottomComponent } from '../calculator-bottom/calculator-bottom.component';
 
 @Component({
@@ -7,7 +7,36 @@ import { CalculatorBottomComponent } from '../calculator-bottom/calculator-botto
   imports: [CalculatorBottomComponent],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handKeyboardEvent($event)',
+  }
 })
 export class CalculatorComponent {
+
+  public calculatorButtons = viewChildren( CalculatorBottomComponent );
+
+  handleClick( key: string ){
+    console.log({ key })
+  }
+
+  handKeyboardEvent( event: KeyboardEvent ){
+
+    const keyEquivalent: Record<string, string> = {
+      Escape: 'C',
+      Clear: 'C',
+      '*': 'x',
+      '/': '÷',
+      Enter: '='
+    }
+
+    const key = event.key;
+    const keyValue = keyEquivalent[key] ?? key;
+
+    this.handleClick( keyValue );
+
+    this.calculatorButtons().forEach((button) => {
+      button.keyboardPressedStyl(keyValue);
+    })
+  }
 
 }
